7858648fdee46209378de100c38d868459f30e56
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-12-24 16:14:11 -0600

    Add debug logging

--- hydro.py
+++ hydro.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import requests
 import psycopg2
 
@@ -8,16 +9,24 @@ sensors = {
     'T8': 'http://storm.chordsrt.com/instruments/13.json?last',
     'T9': 'http://storm.chordsrt.com/instruments/14.json?last',
 }
+logging.debug('sensors: %s', repr(sensors))
 
 conn = psycopg2.connect(dbname='xxxxxx', user='xxxxxxxx',
                         password='xxxxxxxx', host='xxx.xxx.xxx.xxx')
+logging.debug('conn: %s', repr(conn))
+
 curs = conn.cursor()
+logging.debug('curs: %s', repr(curs))
 
 for device, url in sensors.items():
+    logging.debug('device, url: %s, %s', repr(device), repr(url))
     res = requests.get(url)
+    logging.debug('res: %s', repr(res))
     jsonResult = json.loads(res.content)
+    logging.debug('jsonResult: %s', repr(jsonResult))
     time = jsonResult['Data']['Time'][0]
     value = jsonResult['Data']['depth_sonic'][0]
+    logging.debug('time, value: %s, %s', repr(time), repr(value))
     update_query = "update latest_readings set taken_at = %s, current_value = %s where device_serial = %s"
     curs.execute(update_query, (time, value, device))
     conn.commit()
