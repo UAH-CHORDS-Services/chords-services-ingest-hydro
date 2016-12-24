e1ed6915902db56066736a43ebc9af86540f3415
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-12-24 14:46:51 -0600

    Use single string for SQL

--- hydro.py
+++ hydro.py
@@ -18,8 +18,6 @@ for device, url in sensors.items():
     jsonResult = json.loads(res.content)
     time = jsonResult['Data']['Time'][0]
     value = jsonResult['Data']['depth_sonic'][0]
-    update_query = "update latest_readings set taken_at = %s" + \
-        ", current_value = %s" + \
-        " where device_serial = %s"
+    update_query = "update latest_readings set taken_at = %s, current_value = %s where device_serial = %s"
     curs.execute(update_query, (time, value, device))
     conn.commit()
